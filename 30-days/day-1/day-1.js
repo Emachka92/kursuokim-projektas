75354638bbfe8af5e2f8a81dc8f62f97055f8637
Emachka92 <ema.kauniete@gmail.com>
2026-02-02 17:28:33 +0200

    js is chatgpt logikos ir pratimu

---
+++ 30-days/day-1/day-1.js
@@ -0,0 +1,27 @@
+// const temperatura = 15;
+
+// if (temperatura >= 20) {
+//     console.log("karšta");
+// } else if (temperatura >=10) {
+//     console.log("šilta");
+// } else {
+//     console.log("šalta");
+// }
+
+// const amzius = 16;
+// const turiTevuLeidima = true;
+
+// if (amzius >= 18 || turiTevuLeidima) {
+//     console.log("ileidziama");
+// } else {
+//     console.log("neileidziama");
+// }
+
+const amzius = 20;
+const yraStudentas = false;
+
+if ((amzius >= 18 && yraStudentas) || amzius >= 65) {
+    console.log("ileidziama");
+} else{
+    console.log("neileidziama");
+}
